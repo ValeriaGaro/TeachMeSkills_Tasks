bf9282478c301983606baf7948b9ddf81c4bc7dd
ValeriaG <68424407+ValeriaGaro@users.noreply.github.com>
2022-03-21 21:26:55 +0300

    Adding names of a months to list

--- TMS_Task1/task3.py
+++ TMS_Task1/task3.py
@@ -6,7 +6,7 @@ user_dictionary = {"Course": "Python development", "School": "TeachMeSkills"}
 user_integer = 22
 user_float = 3.2
 user_list = ["Alex", "Sam", 8, 2.5]
-user_tuple = ("January", "February", "March")
+user_tuple = ("January", "February", "March", "June", "July")
 user_set = {1, 2, 3, 5}
 types_of_data =[user_None_type, user_boolean, user_string, user_dictionary, user_integer, user_float, user_list,
                 user_tuple, user_set]
