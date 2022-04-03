f7f4ed0292d13c042d9bb9b21402d2bcf6e7fed2
valeria <valeriyagaro@gmail.com>
2022-04-03 23:48:44 +0300

    final version of dop task

--- TMS_Task5/dop_task.py
+++ TMS_Task5/dop_task.py
@@ -6,7 +6,7 @@ def check_registration(func):
             for letter in some_password:
                 if letter in special_symbols:
                     func(some_login, some_password)
-
+                    print("Вы зарегистрированы")
         else:
             print("Проверьте вводимые данные! пароль не меньше 8, почту и наличие спец символов")
     return wrapper
@@ -14,22 +14,20 @@ def check_registration(func):
 
 @check_registration
 def create_new_accaunt(login: str, password: str):
-    file_database = open('users.txt', 'r+')
-    file_database.write(f"{login}: {password} ")
-    print("Вы зарегистрированы")
-    file_database.close()
+    with open('users.txt', 'a') as file_database:
+        file_database.write(f"{login}: {password}\n")
 
 
 def log_in_into_accaunt():
-    file_database = open('users.txt', 'r')
-    login = input("Введите Ваш логин: ")
-    password = input("Введите Ваш пароль: ")
-    combination = f"{login}: {password} "
-    if combination in file_database:
-        print('Вы вошли в систему!')
-    else:
-        print("Данных нет в системе")
-    file_database.close()
+    with open('users.txt', 'r') as file_database:
+        login = input("Введите Ваш логин: ")
+        password = input("Введите Ваш пароль: ")
+        combination = f"{login}: {password}"
+        for line in file_database:
+            if combination in line:
+                print('Вы вошли в систему!')
+            else:
+                print("Данных нет в системе")
 
 
 user_choice = input("Войти / Регистрация: ")
