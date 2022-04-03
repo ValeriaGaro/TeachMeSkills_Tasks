dc3207b12ba7e64c6c41ee909f2942dc81a6d013
valeria <valeriyagaro@gmail.com>
2022-04-03 19:13:15 +0300

    Editing dop_task with decorator

--- TMS_Task5/dop_task.py
+++ TMS_Task5/dop_task.py
@@ -1,30 +1,30 @@
-#def check_password(func):
-#   def wrapper():
-#        func()
+def check_registration(func):
+    def wrapper(some_login, some_password):
+        special_symbols = ["!", "?", "_", "*"]
+        domain_checking = "@yandex.by"
+        if len(some_password) >= 8 and some_login[-10:] == domain_checking:
+            for letter in some_password:
+                if letter in special_symbols:
+                    func(some_login, some_password)
+
+        else:
+            print("Проверьте вводимые данные! пароль не меньше 8, почту и наличие спец символов")
+    return wrapper
 
 
-def create_new_accaunt():
-    user_login = input("Введите логин(Учтите, мы очень любим почты yandex:) Другие не признаем): ")
-    user_password = input("Введите пароль: ")
+@check_registration
+def create_new_accaunt(login: str, password: str):
     file_database = open('users.txt', 'r+')
-    special_symbols = ["!", "?", "_", "*"]
-    domain_checking = "@yandex.by"
-    if len(user_password) >= 8 and user_login[-10:] == domain_checking:
-        for letter in user_password:
-            if letter in special_symbols:
-                file_database.write(f"{user_login}: {user_password}")
-                print("Вы зарегистрированы")
-    else:
-        print("Проверьте вводимые данные! пароль не меньше 8, почту и наличие спец символов")
+    file_database.write(f"{login}: {password} ")
+    print("Вы зарегистрированы")
     file_database.close()
 
 
-
 def log_in_into_accaunt():
-    user_login = input("Введите логин: ")
-    user_password = input("Введите пароль: ")
     file_database = open('users.txt', 'r')
-    combination = f"{user_login}: {user_password}"
+    login = input("Введите Ваш логин: ")
+    password = input("Введите Ваш пароль: ")
+    combination = f"{login}: {password} "
     if combination in file_database:
         print('Вы вошли в систему!')
     else:
@@ -35,10 +35,10 @@ def log_in_into_accaunt():
 user_choice = input("Войти / Регистрация: ")
 if user_choice == "Войти":
     log_in_into_accaunt()
-    pass
 elif user_choice == "Регистрация":
-    create_new_accaunt()
-    pass
+    user_login = input("Введите Ваш логин: ")
+    user_password = input("Введите Ваш пароль: ")
+    create_new_accaunt(user_login, user_password)
 else:
     print("Проверьте Ваш запрос.")
 
