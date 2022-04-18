44f3acd16c80e459e4dd00a92b17704ce2f74e21
valeria <valeriyagaro@gmail.com>
2022-04-18 21:17:01 +0300

    Updated name of a class

--- TMS_Task8/Task8.py
+++ TMS_Task8/Task8.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Electronic_Cigarettes():
+class ElectronicCigarette():
     minimal_power = 8
     name: str
     power: int
@@ -24,15 +24,11 @@ class Electronic_Cigarettes():
 
 
 
-
-
-vap_xros = Electronic_Cigarettes("Vaporesso XROS", 16, 16513216)
-vap_barr = Electronic_Cigarettes("Vaporesso Barr", 13, 15644424)
-elf = Electronic_Cigarettes("Elf Bar", 12, 19849652)
-
+vap_xros = ElectronicCigarette("Vaporesso XROS", 16, 16513216)
+vap_barr = ElectronicCigarette("Vaporesso Barr", 13, 15644424)
+elf = ElectronicCigarette("Elf Bar", 12, 19849652)
 print(vap_xros)
-print(Electronic_Cigarettes.compare_power(vap_barr, elf))
+print(ElectronicCigarette.compare_power(vap_barr, elf))
 print(vap_barr.minimal_power)
-
 vap_barr.change_default_value(10)
 print(vap_barr.minimal_power)
